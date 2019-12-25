72dd770bbdb4040db3fd6ee3bbb3e30cd640fd6e
Pranav Vashi <neobuddy89@gmail.com>
2019-12-25 22:06:54 +0530

    overlays: Add back IconShapeSquareOverlay
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -53,6 +53,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedThemePickerOverlay \
 	IconShapePebbleOverlay \
 	IconShapeRoundedRectOverlay \
+	IconShapeSquareOverlay \
 	IconShapeSquircleOverlay \
 	IconShapeTaperedRectOverlay \
 	IconShapeTeardropOverlay \
