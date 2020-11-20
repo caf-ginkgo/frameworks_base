85351cce8c5d63024944d00010076bad7f388b88
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-20 11:28:50 +0530

    Revert "QS: Use Settings.Panel intents for WiFi and NFC"
    This reverts commit f8413eacc9892550eaa84ee7ec3dee7714e14984.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
@@ -80,7 +80,7 @@ public class NfcTile extends QSTileImpl<BooleanState> {
 
     @Override
     public Intent getLongClickIntent() {
-        return new Intent(Settings.Panel.ACTION_NFC);
+        return new Intent(Settings.ACTION_NFC_SETTINGS);
     }
 
     @Override
